26acd2dc1464c524a3732dab89d2caf91ad81f2a
Hazem Kasim <hazemkasim3@gmail.com>
2020-11-19 02:50:21 +0200

    Added Exam/Competition Page with small edits nvbar

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,14 @@ router.get('/', (req, res) => {
     res.render('home', { errors });
 })
 
+router.get('/competitions', (req, res) => {
+    const errors = [];
+    res.render('competition', { errors });
+})
+
+router.get('/exams', (req, res) => {
+    const errors = [];
+    res.render('exam', { errors });
+})
+
 module.exports = router;
