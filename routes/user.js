7f7f19f6bb19ca230f00cd34687789c7e079a0b9
abdullahalshawafi <abdullahadel.aam@gmail.com>
2020-11-23 00:39:05 +0200

    Added user profile page

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,10 @@
 const router = require("express").Router();
 
+router.get('/:id', (req, res) => {
+    const errors = [];
+    res.render("user-profile", { errors });
+});
+
 router.get('/login', (req, res) => {
     const errors = [];
     res.render("login", { errors });
