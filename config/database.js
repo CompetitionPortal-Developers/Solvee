233c80131cd8b4718f7d5f7c78f6729915183752
abdullahalshawafi <abdullahadel.aam@gmail.com>
2021-10-04 14:54:04 +0200

    Add DB Port

--- config/database.js
+++ config/database.js
@@ -5,6 +5,7 @@ const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
     database: process.env.DATABASE
 });
 
